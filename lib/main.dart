3c34d8d62e69c727c5a165ffaa4a013a3bc338b4
MHMD <mohammad.2003827@gmail.com>
2024-12-20 10:39:27 +0300

    added firebase_core firebase_auth cloud_firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:aabu/themes/dark_theme.dart';
 import 'package:aabu/themes/light_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AabuApp());
 }
 
